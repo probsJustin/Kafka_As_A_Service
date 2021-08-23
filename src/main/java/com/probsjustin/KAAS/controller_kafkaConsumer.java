4e1ddcc65325f61cb7cdc7ebd9572eae2aed82c4
Justin Hagerty <wheelsmanx@gmail.com>
2021-08-22 20:58:01 -0400

    added request validation

--- src/main/java/com/probsjustin/KAAS/controller_kafkaConsumer.java
+++ src/main/java/com/probsjustin/KAAS/controller_kafkaConsumer.java
@@ -1,5 +1,30 @@
 package com.probsjustin.KAAS;
 
-public class controller_kafkaConsumer {
+import java.util.Map;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class controller_kafkaConsumer {
+	String kafkaHost; 
+	String kafakTopic; 
+	String kafkaMessage;
+	controller_kafkaConsumer(){
+		
+	}
+	
+	Map <String,String> checkRequestParameter_Validator(HttpServletRequest func_request, String func_parameter_to_check) {
+		Map<String,String> returnMap = null;
+		if(func_request.getParameter(func_parameter_to_check) != null) {
+			returnMap.put("bool", "true");
+			returnMap.put("parameter_value", func_request.getParameter(func_parameter_to_check));
+		}else {
+			returnMap.put("bool", "false");
+		}
+		return returnMap;
+		
+	}
 }
--- src/main/java/com/probsjustin/KAAS/controller_kafkaProducer.java
+++ src/main/java/com/probsjustin/KAAS/controller_kafkaProducer.java
@@ -1,5 +1,7 @@
 package com.probsjustin.KAAS;
 
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,8 +9,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class controller_kafkaProducer {
-
+	String kafkaHost; 
+	String kafakTopic; 
+	String kafkaMessage; 
 	controller_kafkaProducer(){
+		
+	}
+	
+	Map <String,String> checkRequestParameter_Validator(HttpServletRequest func_request, String func_parameter_to_check) {
+		Map<String,String> returnMap = null;
+		if(func_request.getParameter(func_parameter_to_check) != null) {
+			returnMap.put("bool", "true");
+			returnMap.put("parameter_value", func_request.getParameter(func_parameter_to_check));
+		}else {
+			returnMap.put("bool", "false");
+		}
+		return returnMap;
 		
 	}
 }
