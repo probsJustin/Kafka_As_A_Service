b7ca88439eaaf85233a49091b2dab18cb7dd4239
Justin Hagerty <wheelsmanx@gmail.com>
2021-08-24 12:43:11 -0400

    added clean param checking to controller producer

--- src/main/java/com/probsjustin/KAAS/controller_kafkaProducer.java
+++ src/main/java/com/probsjustin/KAAS/controller_kafkaProducer.java
@@ -1,5 +1,6 @@
 package com.probsjustin.KAAS;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -16,20 +17,43 @@ public class controller_kafkaProducer {
 		
 	}
 	
+
 	Map <String,String> checkRequestParameter_Validator(HttpServletRequest func_request, String func_parameter_to_check) {
 		Map<String,String> returnMap = null;
 		if(func_request.getParameter(func_parameter_to_check) != null) {
 			returnMap.put("bool", "true");
 			returnMap.put("parameter_value", func_request.getParameter(func_parameter_to_check));
+			returnMap.put("parameter_key", func_parameter_to_check);
 		}else {
 			returnMap.put("bool", "false");
 		}
-		return returnMap;	
+		return returnMap;
+		
+	}
+	
+	Boolean check_requestParam_Map(List<Map<String, String>> temp_list) {
+		Boolean returnBool = true; 
+		
+		for (Map<String, String> tempFunc : temp_list){
+			if(tempFunc.get("bool") == "false") {
+				returnBool = false; 
+			}
+		}
+		return returnBool;	
 	}
 	
 	void controller(HttpServletRequest func_request, HttpServletResponse func_response) {
+
+		List<Map<String,String>> temp_list = null; 	
 		
-		
+		temp_list.add(this.checkRequestParameter_Validator(func_request, "topic"));
+		temp_list.add(this.checkRequestParameter_Validator(func_request, "address")); 
+		temp_list.add(this.checkRequestParameter_Validator(func_request, "message"));
+
+		if(check_requestParam_Map(temp_list)) {
+			
+		}
+ 		
 	}
 	
 }
