5e97aecd01d2948be542e3a716827efcc4c77d2d
Justin Hagerty <wheelsmanx@gmail.com>
2021-08-26 23:48:34 -0400

    finished returnable objects for now

--- src/main/java/com/probsjustin/KAAS/returnObject.java
+++ src/main/java/com/probsjustin/KAAS/returnObject.java
@@ -14,7 +14,7 @@ public class returnObject<T> {
 		Boolean func_bool_return = false; 
 		this.bool = func_bool; 
 		if(this.bool == func_bool) {
-			logger.trace("The returnObject of [" + this.info + "] has been set to -> " + this.bool);
+			logger.trace("This instance of returnObject of [" + this.info + "] has set its bool variable to -> " + this.bool);
 			func_bool_return = true; 
 		}else {
 			func_bool_return = false; 
@@ -26,7 +26,35 @@ public class returnObject<T> {
 		Boolean func_bool_return = false;
 		this.info = func_info; 
 		if(this.info == func_info) {
-			
+			logger.trace("This instance of returnObject has been set to -> " + this.info);
+			func_bool_return = true;
+		}else {
+			func_bool_return = false; 
 		}
+		return func_bool_return;
+	}
+	
+	Boolean setMessage(String func_message) {
+		Boolean func_bool_return = false;
+		this.message = func_message; 
+		if(this.message == func_message) {
+			logger.trace("This instance of returnObject ["+ this.info +"] has set its message variable to -> " + this.message);
+			func_bool_return = true;
+		}else {
+			func_bool_return = false; 
+		}
+	return func_bool_return;
+	}
+	
+	Boolean setObject(Object func_object) {
+		Boolean func_bool_return = false;
+		this.instance_returnable = func_object; 
+		if(this.instance_returnable == func_object) {
+			logger.trace("This instance of returnObject ["+ this.info +"] has set its instance_returnable variable to -> " + this.instance_returnable);
+			func_bool_return = true;
+		}else {
+			func_bool_return = false; 
+		}
+	return func_bool_return;
 	}
 }
