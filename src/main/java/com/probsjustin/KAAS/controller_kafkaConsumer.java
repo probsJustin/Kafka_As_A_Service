9ccfb295e362205bd7702fe50d414b1a1d14056d
Justin Hagerty <wheelsmanx@gmail.com>
2021-08-28 11:05:49 -0400

    changed the kafkaConsumer equest to match the producer request for the param check

--- src/main/java/com/probsjustin/KAAS/controller_kafkaConsumer.java
+++ src/main/java/com/probsjustin/KAAS/controller_kafkaConsumer.java
@@ -2,6 +2,7 @@ package com.probsjustin.KAAS;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,16 +17,19 @@ import org.apache.logging.log4j.Logger;
 
 public class controller_kafkaConsumer {
 	logger_internal instance_logger_internal = new logger_internal(); 
-
+	List<String> expectedRequestParamList = new ArrayList(); 
+	
 	controller_kafkaConsumer(){
 		instance_logger_internal.debug("controller_kafkaConsumer instance created");
+		this.expectedRequestParamList.add("topic");
+		this.expectedRequestParamList.add("address");
 	}
 	
 	returnObject<String> checkRequestParameter_Validator(HttpServletRequest func_request, String func_parameter_to_check) {
 		returnObject<String> returnObjectInstance = new returnObject();
 		if(func_request.getParameter(func_parameter_to_check) != null) {
 			returnObjectInstance.setBool(true);
-			returnObjectInstance.setObject(func_request.getParameter(func_parameter_to_check));
+			returnObjectInstance.setObject(func_request.getParameter(func_parameter_to_check).toString());
 			returnObjectInstance.setInfo(func_parameter_to_check);
 		}else {
 			returnObjectInstance.setBool(false);
@@ -35,41 +39,39 @@ public class controller_kafkaConsumer {
 		
 	}
 	
-	Boolean check_requestParam_Map(List<returnObject<String>> temp_list) {
+	Boolean check_requestParam_Map(Map <String,returnObject<String>> temp_list) {
 		Boolean returnBool = true; 
-		
-		for (returnObject<String> tempFunc : temp_list){
-			if(tempFunc.bool == false) {
-				returnBool = false; 
+		for (String temp_itr : this.expectedRequestParamList){
+			if(!temp_list.get(temp_itr).bool){
+				returnBool = false;
 			}
+			instance_logger_internal.debug(temp_list.get(temp_itr).info);
 		}
 		return returnBool;	
 	}
 	
-	returnObject check_missingParameter(List<returnObject<String>> temp_list) {
+	returnObject check_missingParameter(Map <String,returnObject<String>> temp_list) {
 		returnObject instance_returnObject = new returnObject();
 		String tempString = ""; 
-		for (returnObject<String> tempFunc : temp_list){
-			if(tempFunc.bool == false) {
-				tempString += tempFunc.info + ", "; 
+		for (String temp_itr : this.expectedRequestParamList){
+			if(!temp_list.get(temp_itr).bool){
+				tempString += temp_itr + ", ";
 			}
 		}
-		tempString = tempString.substring(0, tempString.length()-2);
-		instance_returnObject.setObject(tempString);
+		instance_returnObject.setObject(tempString.toString().substring(0, tempString.length()-2));
 		return instance_returnObject; 
 	}
 	
 	HttpServletResponse controller(HttpServletRequest func_request, HttpServletResponse func_response, identification_request_holder func_identification_request_holder_instance) throws IOException {
 
-		List<returnObject<String>> temp_list = new ArrayList(); 	
+		Map <String,returnObject<String>> temp_map_holder_request_parameters_of_returnObject = new HashMap<String,returnObject<String>>(); 	
 		instance_logger_internal.debug(func_identification_request_holder_instance.getRequest_ID_String()); 
 		instance_logger_internal.debug(func_identification_request_holder_instance.getRequest_ID_String() + " Checking All request parameters via temp_list");
 
-		temp_list.add(this.checkRequestParameter_Validator(func_request, "topic"));
-		temp_list.add(this.checkRequestParameter_Validator(func_request, "address")); 
-		temp_list.add(this.checkRequestParameter_Validator(func_request, "batchSize"));
+		temp_map_holder_request_parameters_of_returnObject.put("topic", this.checkRequestParameter_Validator(func_request, "topic"));
+		temp_map_holder_request_parameters_of_returnObject.put("address", this.checkRequestParameter_Validator(func_request, "address")); 
 
-		if(check_requestParam_Map(temp_list)) {
+		if(check_requestParam_Map(temp_map_holder_request_parameters_of_returnObject)) {
 			try {
 				func_response.getWriter().append("the things are there that need to be there");
 			} catch (IOException e) {
@@ -79,9 +81,8 @@ public class controller_kafkaConsumer {
 		}else {
 			instance_logger_internal.debug(func_identification_request_holder_instance.getRequest_ID_String() + " Request parameters are missing from the check_requestPAram_map");
 			func_response.getWriter().append("The request is malformed and missing some parameters to fufill your request.");
-			func_response.getWriter().append("Parameter missing: " + check_missingParameter(temp_list).instance_returnable.toString());
+			func_response.getWriter().append("\n Parameter missing: " + check_missingParameter(temp_map_holder_request_parameters_of_returnObject).instance_returnable);
 		}
 		return func_response;
- 		
 	}
 }
