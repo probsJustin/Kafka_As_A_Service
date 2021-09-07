22bf897be712a7c83235fe79d689273de1732c6a
Justin Hagerty <wheelsmanx@gmail.com>
2021-09-06 20:33:37 -0400

    added note to build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 ls -la 
 ../../java/apache-maven-3.8.1/bin/mvn package -f ./pom.xml
 ls -la
-
+# might want to change these to your docker instances/tags
 ../../Program\ Files/Docker/Docker/resources/bin/docker.exe build -t justinshagerty/kafka_as_a_service .
 ../../Program\ Files/Docker/Docker/resources/bin/docker.exe run -it -p 80:80 justinshagerty/kafka_as_a_service
 
