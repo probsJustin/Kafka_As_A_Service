45345680cc80e39e320cc076ba459fc0c3729689
Justin Hagerty <wheelsmanx@gmail.com>
2021-10-12 18:16:40 -0400

    fixed docker command in build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ ls -la
 ../../java/apache-maven-3.8.1/bin/mvn package -f ./pom.xml
 ls -la
 # might want to change these to your docker instances/tags
-../../Program\ Files/Docker/Docker/resources/bin/docker.exe build -t justinshagerty/kafka_as_a_service .
-../../Program\ Files/Docker/Docker/resources/bin/docker.exe run -it -p 80:80 justinshagerty/kafka_as_a_service
+docker build -t justinshagerty/kafka_as_a_service .
+docker run -it -p 80:80 justinshagerty/kafka_as_a_service
 
 
