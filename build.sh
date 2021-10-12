8e8456b480af42dc0b7b3cb401c195b0f865f8e0
Justin Hagerty <wheelsmanx@gmail.com>
2021-10-12 18:29:55 -0400

    add docker path because docker cant be found -.-

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ ls -la
 ../../java/apache-maven-3.8.1/bin/mvn package -f ./pom.xml
 ls -la
 # might want to change these to your docker instances/tags
-docker build -t justinshagerty/kafka_as_a_service .
-docker run -it -p 80:80 justinshagerty/kafka_as_a_service
+/usr/bin/docker build -t justinshagerty/kafka_as_a_service .
+/usr/bin/docker run -it -p 80:80 justinshagerty/kafka_as_a_service
 
 
